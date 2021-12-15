cee7a2bbe0448c5ae09f8bfc1ce99d970574ea7f
Eryk Szczepaniec <szczepanieceryk@gmial.com>
2021-12-15 16:44:47 +0100

    change of img src's for gh-pages

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -7,29 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
 
+  // content foe articles component (ngFor loop)
   title:string = "Latest Articles";
-
   articlesContent:Array<any> = [
     {
-      img:'../../assets/images/image-currency.jpg',
+      img:'./assets/images/image-currency.jpg',
       author:'By Claire Robinson',
       title:'Receive money in any currency with no fees',
       text:'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …'
     },
     {
-      img:'../../assets/images/image-restaurant.jpg',
+      img:'./assets/images/image-restaurant.jpg',
       author:'By Wilson Hutton',
       title:'Treat yourself without worrying about money',
       text:'Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …'
     },
     {
-      img:'../../assets/images/image-plane.jpg',
+      img:'./assets/images/image-plane.jpg',
       author:'By Wilson Hutton',
       title:'Take your Easybank card wherever you go',
       text:'We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …'
     },
     {
-      img:'../../assets/images/image-confetti.jpg',
+      img:'./assets/images/image-confetti.jpg',
       author:'By Claire Robinson',
       title:'Our invite-only Beta accounts are now live!',
       text:'After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...'
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -14,22 +14,22 @@ export class MainComponent implements OnInit {
     // content for main-component ngFor loop
     mainContent:Array<any> = [
     {
-      img:'../../assets/images/icon-online.svg',
+      img:'./assets/images/icon-online.svg',
       title:'Online Banking',
       description:'Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.'
     },
     {
-      img:'../../assets/images/icon-budgeting.svg',
+      img:'./assets/images/icon-budgeting.svg',
       title:'Simple Budgeting',
       description:'See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.'
     },
     {
-      img:'../../assets/images/icon-onboarding.svg',
+      img:'./assets/images/icon-onboarding.svg',
       title:'Fast Onboarding',
       description:'We don’t do branches. Open your account in minutes online and start taking control of your finances right away.'
     },
     {
-      img:'../../assets/images/icon-api.svg',
+      img:'./assets/images/icon-api.svg',
       title:'Open API',
       description:'Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.'
     },
